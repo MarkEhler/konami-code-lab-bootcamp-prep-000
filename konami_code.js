0fd0fc3932876f0b17ec2d34db597bc30f1b3f54
Mark Ehler <ehlerm42@gmail.com>
2018-10-17 07:00:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,24 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
+38, 38, 40, 40, 37, 39, 37, 39, 66, 65
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+ 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+ // const key = e.key;
+  var key = parseInt(e.which || e.detail);
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
 }
